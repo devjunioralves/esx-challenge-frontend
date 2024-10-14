325639d407978ebd49767683ea68d36a8f819e3b
Junior Trindade <jrwanderley17@gmail.com>
2024-10-14 10:20:33 -0300

    feat: add service to send API request

---
+++ src/services/urlService.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8000/api/urls/";
+
+export const getUrls = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
+
+export const saveUrl = async (url: string) => {
+  const response = await axios.post(API_URL, { url });
+  return response.data;
+};
+
+export const deleteUrl = async (id: string) => {
+  const response = await axios.delete(`${API_URL}${id}`);
+  return response.data;
+};
